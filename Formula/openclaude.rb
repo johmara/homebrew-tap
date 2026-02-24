304263ec15a5c999acbd79b7583942af39ce79ed
Johan Martinson <johan.t.martinson@gmail.com>
2026-02-24 14:19:55 +0100

    Update openclaude to 0.1.0

--- Formula/openclaude.rb
+++ Formula/openclaude.rb
@@ -7,22 +7,22 @@ class Openclaude < Formula
   on_macos do
     on_arm do
       url "https://github.com/johmara/openclaude/releases/download/v0.1.0/openclaude-darwin-arm64.tar.gz"
-      sha256 "b60a28f10ad36e84441fd6cb990a1a3ccb07ad0fad9586700b063079035af835"
+      sha256 "110432f087f0e2b244337ca5860242f15adb20871b28241db1c1009aa29a743f"
     end
     on_intel do
       url "https://github.com/johmara/openclaude/releases/download/v0.1.0/openclaude-darwin-amd64.tar.gz"
-      sha256 "1a5bbe9b90fb643e8b2ef37b388cd594d9c4bf6a9790986f8a2c634ae920e907"
+      sha256 "001325cc661eee6e23f0b5b13fb4a65948316c5130c5261ed57251add64eea30"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/johmara/openclaude/releases/download/v0.1.0/openclaude-linux-arm64.tar.gz"
-      sha256 "66944bb9052c1d840c28f6ec53689ed29042be47ffdfa0de46016b5720a4ff8c"
+      sha256 "8150667a040e13facfa4e2de250a4108f5c761c401c4eb20799914ba55f5548a"
     end
     on_intel do
       url "https://github.com/johmara/openclaude/releases/download/v0.1.0/openclaude-linux-amd64.tar.gz"
-      sha256 "9a63f310fcb6f0b01e9d0a86ab030571287e29c4e7bb91ab034ffad49000ee88"
+      sha256 "70d94cdab7dfaf90b11d268db38c191f9d451fde63722674db304cfe3163f940"
     end
   end
 
